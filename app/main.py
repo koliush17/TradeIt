bad392599f14c1540581d8823971fd16220352d7
dornierdo17 <koliush2006@gmail.com>
2025-12-02 13:10:20 +0100

    Add routes to news and price extraction logic

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI 
 from app.database.lifespan import lifespan 
-from app.api import fetch_news
+from app.api import fetch_news, fetch_prices
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(fetch_news.router)
+app.include_router(fetch_prices.router)
 
 
 
