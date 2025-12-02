6d47503703ba927dc876bbc471a2097dc634c527
dornierdo17 <koliush2006@gmail.com>
2025-12-02 13:16:13 +0100

    Add pydantic schema for news

--- app/schemas/news_schema.py
+++ app/schemas/news_schema.py
@@ -5,7 +5,7 @@ class CryptoNewsDataSchema(BaseModel):
     title: str 
     summary: str 
     date_published: date 
-    ticker: str 
+    currency: str 
     relevance_score: float 
     ticker_sentiment_score: float
 
