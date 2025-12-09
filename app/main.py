b5da37051bdf557ce2970b3ddf41e62fea89f289
dornierdo17 <koliush2006@gmail.com>
2025-12-09 12:59:23 +0100

    Add router for intraday endpoint. Add invalid crypto name exception handler

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 from app.database.lifespan import lifespan 
-from app.api import fetch_news, fetch_prices, fetch_fear_greed
-from app.exceptions import ExternalAPIError, DataParsingError
+from app.api import fetch_news, fetch_prices, fetch_fear_greed, fetch_intraday_prices
+from app.exceptions import ExternalAPIError, DataParsingError, InvalidCryptoNameError
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(fetch_news.router)
 app.include_router(fetch_prices.router)
 app.include_router(fetch_fear_greed.router)
+app.include_router(fetch_intraday_prices.router)
+
 
 @app.exception_handler(ExternalAPIError)
 async def external_api_handler(request, exc):
@@ -17,3 +19,7 @@ async def external_api_handler(request, exc):
 @app.exception_handler(DataParsingError)
 async def parsing_handler(request, exc):
     return JSONResponse(status_code=500, content={"error": exc.message})
+
+@app.exception_handler(InvalidCryptoNameError)
+async def invalid_cryptoname_handler(request, exc):
+    return JSONResponse(status_code=400, content={"error": exc.message})   
