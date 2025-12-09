d458d660831d2d55ab4b6a5a211a02a0f63709e6
dornierdo17 <koliush2006@gmail.com>
2025-12-09 13:00:03 +0100

    Refactor code to be able to add intraday prices

--- app/repositories/prices_repository.py
+++ app/repositories/prices_repository.py
@@ -7,13 +7,14 @@ async def insert_prices(conn, prices: List[CryptoPricesDataSchema]):
     async with conn.cursor() as cursor:
         for doc in prices:
             await cursor.execute("""INSERT INTO crypto_prices
-                                    (price_date, currency, time_period, open_price, highest_price,
+                                    (price_date, currency, time_period, interval, open_price, highest_price,
                                     lowest_price, close_price, volume)
 
-                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                                    ON CONFLICT (price_date, currency, time_period) DO NOTHING""",
+                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                                    ON CONFLICT (price_date, currency, time_period, interval) DO NOTHING""",
                                     
-                                    (doc.date_price, doc.currency, doc.time_period, doc.open_price, doc.high,
+                                    (doc.date_price, doc.currency, doc.time_period,
+                                     doc.interval, doc.open_price, doc.high,
                                      doc.low, doc.close, doc.volume))
 
         await conn.commit()
