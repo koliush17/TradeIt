ad353cd722caa8065f75fb7a421af95806e7ca11
dornierdo17 <koliush2006@gmail.com>
2025-12-23 12:38:04 +0100

    Initialize LLM model for an agent

---
+++ agent/utils/llm_model.py
@@ -0,0 +1,19 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from app.schemas.env_schema import settings
+from app.utils.logger import get_logger 
+
+logger = get_logger("main")
+
+def initialize_llm():
+    llm = "gemini-2.5-flash"
+    
+    model = ChatGoogleGenerativeAI(
+        model=llm
+        api_key=settings.GEMINI_API_KEY) 
+
+    logger.info(f"Sucessfully setup a LLM: {llm}")
+
+    return model 
+
+
